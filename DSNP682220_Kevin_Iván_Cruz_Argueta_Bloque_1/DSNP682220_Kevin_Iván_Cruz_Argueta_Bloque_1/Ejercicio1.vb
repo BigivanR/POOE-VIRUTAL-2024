dce960c74a140e806b906162643d3fea2e96504f
BigivanR <kevinivancrux@gmail.com>
2024-09-22 08:43:45 -0600

    Ejercicio 2 Determinar si el número es positivo o negativo.

---
+++ DSNP682220_Kevin_Iván_Cruz_Argueta_Bloque_1/DSNP682220_Kevin_Iván_Cruz_Argueta_Bloque_1/Ejercicio1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+    Sub Main()
+        'Ejercicio 1 solicitar la edad de una persona y determinar si es menor o mayor de edad, deberá mostrar un mensaje.
+        Dim Edad As Integer
+        Dim mayor, menor As Integer
+        mayor = 18
+        menor = 17
+
+        Console.WriteLine("Ingrese su edad: ")
+        Edad = Console.ReadLine()
+
+        If Edad >= mayor Then Console.WriteLine("Es mayor de edad")
+        If Edad <= menor Then Console.WriteLine("Es menor de edad")
+
+        Console.ReadKey() ' pause
+
+    End Sub
+
+End Module
---
+++ DSNP682220_Kevin_Iván_Cruz_Argueta_Bloque_1/DSNP682220_Kevin_Iván_Cruz_Argueta_Bloque_1/Module2.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Globalization
+Imports System.Runtime.InteropServices
+
+Module Module2
+    Sub main()
+        'Determinar si el número es positivo o negativo.
+        Dim num1 As String
+
+        Console.Write("Introduce un número: ")
+        num1 = Console.ReadLine()
+
+        If (num1 >= 1) Then
+
+            Console.WriteLine("El número es positivo.")
+
+        ElseIf (num1 <= -1) Then
+
+            Console.WriteLine("El número es negativo.")
+
+        ElseIf (num1 = 0) Then
+
+            Console.WriteLine("El número es neutro.")
+
+        Else
+
+            Console.WriteLine("Por favor, introduce un número válido.")
+
+
+        End If
+
+    End Sub
+
+End Module
